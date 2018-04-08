f7c2e7d75efc74c4fbe55716a9c72c93d1826de4
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-04-07 23:32:45 -0400

    Feat: Take bus number from user

--- analyze_csv.py
+++ analyze_csv.py
@@ -6,7 +6,11 @@ with open('DART_Bus_Stops.csv', 'rb') as csvfile:
 
     # First row is file header
     header = next(reader)
-    print (" ".join(column for column in header))
+    print (' '.join(column for column in header))
+
+    # Take input from user
+    print ('Please provide bus no to see the stop names for that particular bus!')
+    bus_no = input('Enter the bus number: ' )
 
     # Scan the csv data
     for row in reader:
