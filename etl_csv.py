268b499f37ca6ea178b8523932150d5c07d3cc97
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-04-08 22:31:03 -0400

    Feat: Add field to show approx amount

--- etl_csv.py
+++ etl_csv.py
@@ -3,6 +3,7 @@ table = (
     etl
     .fromcsv('Credit_Card_Transactions_for_FY_2017_by_Department.csv')
     .convert('DEPT_NAME', 'lower')
+    .addfield('APPROX_AMT(K)', lambda row: int(round(float(row.MERCHANDISE_AMT[1:]) / 1000, 0)))
 )
 
 print table
