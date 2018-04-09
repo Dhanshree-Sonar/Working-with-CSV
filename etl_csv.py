0ad1ac73933f253030872f5857fe8d1af45c7481
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-04-08 22:54:39 -0400

    Fix: Show the approximated amount in K format

--- etl_csv.py
+++ etl_csv.py
@@ -1,12 +1,15 @@
 import petl as etl
+
 table = (
     etl
     .fromcsv('Credit_Card_Transactions_for_FY_2017_by_Department.csv')
     .convert('DEPT_NAME', 'lower')
-    .addfield('APPROX_AMT(K)', lambda row: int(round(float(row.MERCHANDISE_AMT[1:]) / 1000, 0)))
+    .addfield('APPROX_AMT_K', lambda row: int(round(float(row.MERCHANDISE_AMT[1:]) / 1000, 0)))
+    .addfield('APPROX_AMT', lambda row: '$' + str(row.APPROX_AMT_K) + 'K')
 )
 
-new_table = table.cut('DEPT_NAME', 'APPROX_AMT(K)')
+new_table = table.cut('DEPT_NAME','APPROX_AMT')
+
 
 print '\nDepartment wise Credit transaction for year 2017'
 print new_table
